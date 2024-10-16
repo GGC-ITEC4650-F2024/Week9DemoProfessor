360be28a7130365d9a7b13c8b921abd60707bc0c
Sebastien Siva <ssiva@ggc.edu>
2024-10-16 09:16:50 -0400

    Lecture 10_16_24

--- Prof_SpaceLaser/Assets/Scripts/CrossHairController.cs
+++ Prof_SpaceLaser/Assets/Scripts/CrossHairController.cs
@@ -6,10 +6,16 @@ public class CrossHairController : MonoBehaviour
 {
     public GameObject exploPrefab;
 
+    public Vector3 laserOffset;
+    public LayerMask crossHairLayer;
+
+    ParticleSystem laserPS;
+
     // Start is called before the first frame update
     void Start()
     {
         Cursor.visible = false;
+        laserPS = GetComponentInChildren<ParticleSystem>();
     }
 
     // Update is called once per frame
@@ -17,13 +23,19 @@ public class CrossHairController : MonoBehaviour
     {
         Ray r = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hitInfo;
-        if(Physics.Raycast(r, out hitInfo)){
+        if(Physics.Raycast(r, out hitInfo, Mathf.Infinity, crossHairLayer)){
             GameObject g = hitInfo.collider.gameObject;
             transform.position = hitInfo.point + 0.1f * Vector3.up;
 
             if(Input.GetButtonDown("Fire1")) {
+                laserPS.transform.LookAt(
+                    Camera.main.transform.position + laserOffset);
+                laserPS.Play();
+
                 GameObject ex = Instantiate(exploPrefab, 
                     transform.position, Quaternion.identity);
+                
+                Destroy(ex, 1);
             }
         }
     }
